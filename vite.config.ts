33853df8847c3d60e04e0f28e46aefa5520ca96a
JAIPRAKASH-07 <your.sjaiprakash682@gmail.com>
2026-01-02 16:34:55 +0530

    fix router nesting error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/vistabuzz/",   // 👈 THIS IS THE KEY
   plugins: [react()],
+    build: {
+  sourcemap: true,
+},
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
+  
+
   },
-  base: "/", // 🔥 MUST MATCH REPO NAME
-})
+});
