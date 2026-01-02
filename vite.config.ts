6dbba3dcc9ba6f4abcc05c3db9c2fb9a912d64f1
VISTABUZZ (JP) <sjaiprakash682@gmail.com>
2026-01-02 17:15:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/vistabuzz/",   // 👈 THIS IS THE KEY
+  base: "/",   // 👈 THIS IS THE KEY
   plugins: [react()],
     build: {
   sourcemap: true,
